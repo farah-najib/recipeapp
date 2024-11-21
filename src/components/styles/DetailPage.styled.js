7d4e0a81639690a6807f472e9e228a87f1e2bfa0
Farah Najib <farahengnajib@gmail.com>
2024-11-21 14:20:46 +0100

    add style for detail meal

---
+++ src/components/styles/DetailPage.styled.js
@@ -0,0 +1,41 @@
+
+import styled from 'styled-components'
+
+// Modal styles
+export const ModalBackdrop = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+`
+
+export const ModalContainer = styled.div`
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    width: 500px;
+    max-height: 80%;
+    overflow-y: auto;
+`
+
+export const CloseButton = styled.button`
+    background-color: red;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+
+    &:hover {
+        background-color: darkred;
+    }
+`
