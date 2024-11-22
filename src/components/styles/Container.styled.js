a8ec88375fd5e122d48a4b1750e4363b1bb38a1b
Farah Najib <farahengnajib@gmail.com>
2024-11-22 11:54:02 +0100

    .

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledContainer = styled.div `
     display: grid;
     grid-template-columns: 1fr;
-    grid-template-rows: 100vh;
+     grid-template-rows: auto;
     place-items: center;
     text-align: center;
 
