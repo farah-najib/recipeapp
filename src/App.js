24e56ea3d8e1629de75cc6cc1ea6c5b990494fb2
Farah Najib <farahengnajib@gmail.com>
2024-11-22 11:54:37 +0100

    add navbar to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext,useMemo } from 'react'
+import React, { useContext, useMemo } from 'react'
 import Meals from './components/Meals'
 import { GlobalStyles } from './components/styles/Global'
 import { StyledContainer } from './components/styles/Container.styled'
@@ -6,23 +6,20 @@ import { lightTheme, darkTheme } from './components/theme'
 import { ThemeProvider } from 'styled-components'
 import { ThemeContext } from './ThemeContext'
 import { SwitchButton } from './components/styles/SwitchButton'
+import Navbar from './components/Navbar'
 
 function App() {
     const { theme, toggleTheme } = useContext(ThemeContext)
-    const themeMemo = useMemo(() => (theme === 'light' ? lightTheme : darkTheme),[theme]);
+    const themeMemo = useMemo(
+        () => (theme === 'light' ? lightTheme : darkTheme),
+        [theme]
+    )
     return (
         <ThemeProvider theme={themeMemo}>
             <>
                 <GlobalStyles />
+                <Navbar checked={theme === 'light'} onChange={toggleTheme} />
 
-                <SwitchButton>
-                    <input
-                        type="checkbox"
-                        checked={theme === 'light'}
-                        onChange={toggleTheme}
-                    />
-                    <span className="slider round" />
-                </SwitchButton>
                 <StyledContainer>
                     <Meals />
                 </StyledContainer>
