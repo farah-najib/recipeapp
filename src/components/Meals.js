05d1b48826180d0aa765d2f5cce1af09d1d225dc
Farah Najib <farahengnajib@gmail.com>
2024-11-22 12:03:04 +0100

    .

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -43,7 +43,7 @@ const Meals = () => {
     const getMeals = useCallback(async () => {
 
         const response = await axios.get(
-            'https:/www.themealdb.com/api/json/v1/1/search.php?f=s'
+            'https:/www.themealdb.com/api/json/v1/1/search.php?f=a'
         )
         if (response.status === 200)
             setDispatch({ type: response.status, data: response.data })
